5657e92c272673d6be24007297cd2a939447d7fc
sbx_user1051 <sbx_user1051@169.254.48.69>
2026-03-26 13:40:15 +0000

    Thu Mar 26 13:40:15 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -13,5 +13,7 @@ def test_enqueue_size_dequeue_flow() -> None:
 
 def test() -> None:
     run_queue([
-        call_enqueue()
+        call_enqueue("logs", 1, iso_ts(delta_minutes=0)).expect(1),
+        call_size().expect(1),
+        call_dequeue().expect("logs", 1),
     ])
