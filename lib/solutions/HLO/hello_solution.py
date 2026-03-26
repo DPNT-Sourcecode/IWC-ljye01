1d7773e10016935ac7d07db4e98be7b9300ade98
sbx_user1051 <sbx_user1051@169.254.48.69>
2026-03-26 13:08:44 +0000

    Thu Mar 26 13:08:44 UTC 2026

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -4,3 +4,7 @@ class HelloSolution:
     # friend_name = unicode string
     def hello(self, friend_name):
         return f"Hello, {friend_name}!"
+
+if __name__ == "__main__":
+    print(HelloSolution().hello("Ekin"))
+
--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -3,3 +3,7 @@ class SumSolution:
     
     def compute(self, x, y):
         return x + y
+
+if __name__ == "__main__":
+    print(SumSolution().compute(1,5))
+
