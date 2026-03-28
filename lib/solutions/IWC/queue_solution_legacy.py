56c32d45bd4dd8a8cecf34138f4adf13463bc1df
sbx_user1051 <sbx_user1051@169.254.34.109>
2026-03-28 13:36:54 +0000

    Sat Mar 28 13:36:54 UTC 2026

--- lib/solutions/IWC/queue_solution_legacy.py
+++ lib/solutions/IWC/queue_solution_legacy.py
@@ -177,8 +177,8 @@ class Queue:
             key=lambda i: (
                 self._priority_for_task(i),
                 self._earliest_group_timestamp_for_task(i),
+                self._timestamp_for_task(i),
                 self._check_bank_statement(i),
-                self._timestamp_for_task(i)
             )
         )
 
@@ -293,5 +293,6 @@ async def queue_worker():
         logger.info(f"Finished task: {task}")
 ```
 """
+
 
 
