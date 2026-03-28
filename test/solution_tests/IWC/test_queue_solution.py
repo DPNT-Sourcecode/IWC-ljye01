438bb1f5d28d0899a6115532e0d2453dfe6e1edf
sbx_user1051 <sbx_user1051@169.254.4.221>
2026-03-28 10:42:22 +0000

    Sat Mar 28 10:42:22 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -60,7 +60,10 @@ def test_unique_with_timestamp() -> None:
 def test_bank_statement_deprio() -> None:
     run_queue([
         call_enqueue(provider="companies_house", user_id=1, timestamp=iso_ts(delta_minutes=0)).expect(1),
-        call_enqueue(provider="bank_statements", user_id=1, timestamp=iso_ts(delta_minutes=0)).expect(1),
-        call_enqueue(provider="id_verification", user_id=1, timestamp=iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue(provider="bank_statements", user_id=1, timestamp=iso_ts(delta_minutes=0)).expect(2),
+        call_enqueue(provider="id_verification", user_id=1, timestamp=iso_ts(delta_minutes=0)).expect(3),
         call_size().expect(3),
+        call_dequeue().expect(provider="companies_house", user_id=1),
+        call_dequeue().expect(provider="id_verification", user_id=1),
+        call_dequeue().expect(provider="bank_statements", user_id=1),
     ])
